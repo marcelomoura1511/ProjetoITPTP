fb07bb2aee3d2b22afacf3a77c9e570b327abd03
marcelomoura1511 <marcelo.moura1511@gmail.com>
2018-11-27 15:37:35 -0300

    RGB para HSV

--- meufiltro.c
+++ meufiltro.c
@@ -1,36 +1,97 @@
 #include "meufiltro.h"
 
 void inicializarWidgetsMeuFiltro() {
-	//widgets das opcoes de filtro
-	widgetControleNivel = 	gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 30, 1);
-	widgetMisturarCanais = gtk_check_button_new_with_label("Misturar canais");
-	g_signal_connect(G_OBJECT(widgetControleNivel), "value-changed", G_CALLBACK(funcaoAplicar), NULL);
 }
 
 void adicionarWidgetsMeuFiltro(GtkWidget *container) {
 
-	GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-	gtk_container_set_border_width(GTK_CONTAINER(vbox), 20);
-	gtk_container_add(GTK_CONTAINER(container), vbox);
-	gtk_container_add(GTK_CONTAINER(vbox), widgetControleNivel);
-	gtk_container_add(GTK_CONTAINER(vbox), widgetMisturarCanais);
+
+}
+
+
+struct RGB
+{
+    unsigned char R;
+    unsigned char G;
+    unsigned char B;
+};
+
+struct HSV
+{
+    double H;
+    double S;
+    double V;
+};
+
+static double Min(double a, double b) {
+    return a <= b ? a : b;
+}
+
+static double Max(double a, double b) {
+    return a >= b ? a : b;
+}
+
+struct HSV RGBToHSV(struct RGB rgb) {
+    double delta, min;
+    double h = 0, s, v;
+
+    min = Min(Min(rgb.R, rgb.G), rgb.B);
+    v = Max(Max(rgb.R, rgb.G), rgb.B);
+    delta = v - min;
+
+    if (v == 0.0)
+        s = 0;
+    else
+        s = delta / v;
+
+    if (s == 0)
+        h = 0.0;
+
+    else
+  
+    {
+        if (rgb.R == v)
+            h = (rgb.G - rgb.B) / delta;
+        else if (rgb.G == v)
+            h = 2 + (rgb.B - rgb.R) / delta;
+        else if (rgb.B == v)
+            h = 4 + (rgb.R - rgb.G) / delta;
+
+        h *= 60;
+
+        if (h < 0.0)
+            h = h + 360;
+    }
+
+    struct HSV hsv;
+    hsv.H = h;
+    hsv.S = s;
+    hsv.V = v / 255;
+
+    return hsv;
 }
 
 Imagem meuFiltro(Imagem origem,Imagem fundo) {
     Imagem destino = alocarImagem(origem);
-    if(origem.h == fundo.h && origem.w == fundo.w){
-        for(int a = 0;a<origem.h;a++){
-            for(int b = 0;b<origem.w;b++){
-                //if(origem.m[a][b][0] >= 0 && origem.m[a][b][0] <= 80  && origem.m[a][b][1] >= 100 && origem.m[a][b][1] <= 255  && origem.m[a][b][3] >= 0 && origem.m[a][b][3] <= 80){
-                if(origem.m[a][b][0] >= 0 && origem.m[a][b][1] == 255 && origem.m[a][b][3] >= 0){
+    for(int a = 0;a<origem.h;a++){
+        for(int b = 0;b<origem.w;b++){
+            struct RGB rgb ={origem.m[a][b][0],origem.m[a][b][1],origem.m[a][b][0]};
+            struct HSV hsv = RGBToHSV(rgb);
+            //if(origem.m[a][b][0] >= 0 && origem.m[a][b][0] <= 80  && origem.m[a][b][1] >= 100 && origem.m[a][b][1] <= 255  && origem.m[a][b][3] >= 0 && origem.m[a][b][3] <= 80){
+            if(hsv.H >= 60 && hsv.H <= 130 && hsv.S >= 0.15 && hsv.V > 0.15){
+                //if((origem.m[a][b][0]*origem.m[a][b][2]) !=0 && (origem.m[a][b][1]*origem.m[a][b][1]) / (origem.m[a][b][0]*origem.m[a][b][2]) >= 1.5){
                     destino.m[a][b][0] = fundo.m[a][b][0];
                     destino.m[a][b][1] = fundo.m[a][b][1];
                     destino.m[a][b][2] = fundo.m[a][b][2];
-                }else{
-                    destino.m[a][b][0] = origem.m[a][b][0];
-                    destino.m[a][b][1] = origem.m[a][b][1];
-                    destino.m[a][b][2] = origem.m[a][b][2];
-                }
+                /*}else{
+                    destino.m[a][b][0] = (int)(fundo.m[a][b][0]*1.2);
+                    destino.m[a][b][1] = (int)fundo.m[a][b][1];
+                    destino.m[a][b][2] = (int)(fundo.m[a][b][2]*1.2);
+                }*/
+            }else{
+                destino.m[a][b][0] = origem.m[a][b][0];
+                destino.m[a][b][1] = origem.m[a][b][1];
+                destino.m[a][b][2] = origem.m[a][b][2];
             }
         }
     }
